da4e720d37d362090ff0c38640af3a97dad852ff
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-19 21:08:00 +0200

    add play sound feature

--- src/Randomizer/Randomizer.tsx
+++ src/Randomizer/Randomizer.tsx
@@ -1,10 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import LavaSound from "./lava.mp3";
+import NotLavaSound from "./not-lava.mp3";
+import FloorIsSound from "./floor-is.mp3";
 
 export const Randomizer: React.FC = () => {
-  const [minBreak, setMinBreak] = useState<number>(3);
-  const [maxBreak, setMaxBreak] = useState<number>(6);
+  const [minBreak, setMinBreak] = useState<number>(4);
+  const [maxBreak, setMaxBreak] = useState<number>(8);
   const [counter, setCounter] = useState<number>(0);
   const [isLava, setIsLava] = useState<0 | 1>(0);
+
+  const lava = useMemo(() => new Audio(LavaSound), []);
+  const notLava = useMemo(() => new Audio(NotLavaSound), []);
+  const floorIs = useMemo(() => new Audio(FloorIsSound), []);
+
+  useEffect(() => {
+    lava.pause();
+    lava.currentTime = 0;
+
+    notLava.pause();
+    notLava.currentTime = 0;
+
+    if (isLava === 0) {
+      notLava.play();
+    }
+    if (isLava === 1) {
+      lava.play();
+    }
+  }, [counter]);
 
   useEffect(() => {
     const nextBreak = Math.floor(
@@ -13,7 +35,9 @@ export const Randomizer: React.FC = () => {
 
     const nextIsLava = Math.floor(Math.random() * 2) as 0 | 1;
     setIsLava(nextIsLava);
-
+    floorIs.pause();
+    floorIs.currentTime = 0;
+    floorIs.play();
     const timeOutId = setTimeout(() => {
       setCounter((prev) => prev + 1);
     }, nextBreak * 1000);
@@ -28,8 +52,6 @@ export const Randomizer: React.FC = () => {
       <div>
         <p>Max break: {maxBreak} seconds</p>
         <p>Min break: {minBreak} seconds</p>
-        <p>Counter: {counter}</p>
-        <p>Floor is {`${!isLava ? "lava" : "not lava"}`}</p>
       </div>
     </div>
   );
